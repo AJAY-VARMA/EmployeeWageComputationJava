74877b206bf7825f009a27169972e86d16e0dd73
ajay <ajayvarma1101@gmail.com>
2020-04-24 09:48:47 +0530

    [ajay] added usecase to check employee present or not

--- src/main/java/com/employeeWageComputation/EmployeeWageComputation.java
+++ src/main/java/com/employeeWageComputation/EmployeeWageComputation.java
@@ -2,6 +2,15 @@ package com.employeeWageComputation;
 
 public class EmployeeWageComputation {
     public static void main(String[] args) {
+        int PRESENT = 1;
+
         System.out.println("Welcome to Employee Wage Computation");
+
+        int checkPresentOrAbsent = (int) (Math.random() * 2); //check employee present or not using random function
+
+        if (checkPresentOrAbsent == PRESENT)
+            System.out.print("Employee is present");
+        else
+            System.out.print("Employee is absent");
     }
 }
