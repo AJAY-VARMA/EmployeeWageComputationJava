e9e4bcc7321c329febb9f303319b99e53cfe484f
ajay <ajayvarma1101@gmail.com>
2020-04-24 10:46:45 +0530

    [ajay] added usecase to check employee montly wage

--- src/main/java/com/employeeWageComputation/EmployeeWageComputation.java
+++ src/main/java/com/employeeWageComputation/EmployeeWageComputation.java
@@ -4,27 +4,34 @@ public class EmployeeWageComputation {
     public static void main(String[] args) {
         final int FULLTIME = 1;
         final int PARTTIME = 0;
+        int WORKINGDAYS_PER_MONTH = 20;
         int WAGE_PER_HOUR = 20;
         int workingHours = 8;
-        int dailyWage = 0;
+        int montlyWage = 0;
+        int totalDays = 0;
+        int totalHours = 0;
 
         System.out.println("Welcome to Employee Wage Computation");
 
-        int checkWorking = (int) (Math.random() * 3);
+        while (totalDays < WORKINGDAYS_PER_MONTH) {
+            totalDays++;
 
-        switch (checkWorking) {
-            case FULLTIME:
-                workingHours = 8;
-                break;
-            case PARTTIME:
-                workingHours = 4;
-                break;
-            default:
-                workingHours = 0;
+            int checkWorking = (int) (Math.random() * 3);
+
+            switch (checkWorking) {
+                case FULLTIME:
+                    workingHours = 8;
+                    break;
+                case PARTTIME:
+                    workingHours = 4;
+                    break;
+                default:
+                    workingHours = 0;
+            }
+            totalHours += workingHours;
         }
 
-        dailyWage = (WAGE_PER_HOUR * workingHours);
-        System.out.println("Daily Employee Wage:" + dailyWage);
-
+        montlyWage = (WAGE_PER_HOUR * totalHours);
+        System.out.println("Montly Employee Wage:" + montlyWage);
     }
 }
