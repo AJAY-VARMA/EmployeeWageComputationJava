0631e91ba682221f2865a3c700cb9b3f6d435d53
ajay <ajayvarma1101@gmail.com>
2020-04-24 09:56:42 +0530

    [ajay] added usecase to check employee daily wage

--- src/main/java/com/employeeWageComputation/EmployeeWageComputation.java
+++ src/main/java/com/employeeWageComputation/EmployeeWageComputation.java
@@ -3,14 +3,19 @@ package com.employeeWageComputation;
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         int PRESENT = 1;
+        int WAGE_PER_HOUR = 20;
+        int WORKING_HOURS = 8;
+        int dailyWage = 0;
 
         System.out.println("Welcome to Employee Wage Computation");
 
         int checkPresentOrAbsent = (int) (Math.random() * 2); //check employee present or not using random function
 
-        if (checkPresentOrAbsent == PRESENT)
-            System.out.print("Employee is present");
-        else
+        if (checkPresentOrAbsent == PRESENT) {
+            dailyWage = (WAGE_PER_HOUR * WORKING_HOURS);
+            System.out.println("Daily Employee Wage:" + dailyWage);
+        } else {
             System.out.print("Employee is absent");
+        }
     }
 }
