c86355d54e24ec5df8a7b3745e5a4bbd9dd5193d
ajay <ajayvarma1101@gmail.com>
2020-04-24 10:10:50 +0530

    [ajay] added usecase to check employee working fulltime or parttime

--- src/main/java/com/employeeWageComputation/EmployeeWageComputation.java
+++ src/main/java/com/employeeWageComputation/EmployeeWageComputation.java
@@ -3,8 +3,9 @@ package com.employeeWageComputation;
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         int PRESENT = 1;
+        int FULLTIME = 1;
         int WAGE_PER_HOUR = 20;
-        int WORKING_HOURS = 8;
+        int workingHours = 8;
         int dailyWage = 0;
 
         System.out.println("Welcome to Employee Wage Computation");
@@ -12,7 +13,15 @@ public class EmployeeWageComputation {
         int checkPresentOrAbsent = (int) (Math.random() * 2); //check employee present or not using random function
 
         if (checkPresentOrAbsent == PRESENT) {
-            dailyWage = (WAGE_PER_HOUR * WORKING_HOURS);
+
+            int checkWorking = (int) (Math.random() * 2);
+
+            if (checkWorking == FULLTIME)
+                workingHours = 8;
+            else
+                workingHours = 4;
+
+            dailyWage = (WAGE_PER_HOUR * workingHours);
             System.out.println("Daily Employee Wage:" + dailyWage);
         } else {
             System.out.print("Employee is absent");
