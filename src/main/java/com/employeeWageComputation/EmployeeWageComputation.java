a423b2f068b21ee540abe2b00e11de140f2fc841
ajay <ajayvarma1101@gmail.com>
2020-04-25 14:58:00 +0530

    [ajay] refactor: added function to get hours for montly wage

--- src/main/java/com/employeeWageComputation/EmployeeWageComputation.java
+++ src/main/java/com/employeeWageComputation/EmployeeWageComputation.java
@@ -2,43 +2,42 @@ package com.employeeWageComputation;
 
 public class EmployeeWageComputation {
 
-    public static int employeeWage() {
-        final int FULLTIME = 1;
-        final int PARTTIME = 0;
-        int WORKINGDAYS_PER_MONTH = 20;
+    public static int get_Working_Hours(int checkWorking) {
+        final int FULL_TIME = 1;
+        final int PART_TIME = 0;
+        int workingHours = 0;
+        switch (checkWorking) {
+            case FULL_TIME:
+                workingHours = 8;
+                break;
+            case PART_TIME:
+                workingHours = 4;
+                break;
+            default:
+                workingHours = 0;
+        }
+        return workingHours;
+    }
+
+    public static void main(String[] args) {
+        int WORKING_DAYS_PER_MONTH = 20;
         int WAGE_PER_HOUR = 20;
-        int TOTALHOURS = 100;
-        int workingHours = 8;
+        int TOTAL_HOURS = 100;
         int montlyWage = 0;
         int totalDays = 0;
         int totalHours = 0;
-
+        int workingHours = 8;
         System.out.println("Welcome to Employee Wage Computation");
 
-        while (totalDays < WORKINGDAYS_PER_MONTH && totalHours < TOTALHOURS) {
+        while (totalDays < WORKING_DAYS_PER_MONTH && totalHours < TOTAL_HOURS) {
             totalDays++;
-
             int checkWorking = (int) (Math.random() * 3);
-
-            switch (checkWorking) {
-                case FULLTIME:
-                    workingHours = 8;
-                    break;
-                case PARTTIME:
-                    workingHours = 4;
-                    break;
-                default:
-                    workingHours = 0;
-            }
+            workingHours = get_Working_Hours(checkWorking);
             totalHours += workingHours;
         }
 
         montlyWage = (WAGE_PER_HOUR * totalHours);
-        return montlyWage;
-    }
-
-    public static void main(String[] args) {
-        int employeeWage = employeeWage();
-        System.out.print(employeeWage);
+        System.out.println("Montly Wage :" + montlyWage);
     }
 }
+
